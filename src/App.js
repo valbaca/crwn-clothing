f6ff65b641f18d7dbba29051677f2273d78f0f60
Valentin Baca <valbaca@gmail.com>
2021-06-10 21:33:23 -0700

    Showing basic routing to /hats

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import HomePage from './page/homepage/homepage.component.jsx'
+import {Route} from 'react-router-dom'
 import './App.css'
+
+const HatsPage = () => (
+  <div><h1>HATS PAGE</h1></div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage}/>
+      <Route path="/hats" component={HatsPage}/>
     </div>
   )
 }
