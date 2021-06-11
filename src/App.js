17475e8b199f80f8bc1f2f45743dd94b8f79b47c
Valentin Baca <valbaca@gmail.com>
2021-06-10 21:36:35 -0700

    Using a Switch instead of direct Route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import HomePage from './page/homepage/homepage.component.jsx'
-import {Route} from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import './App.css'
 
 const HatsPage = () => (
-  <div><h1>HATS PAGE</h1></div>
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
 )
 
 function App() {
   return (
     <div>
-      <Route exact path="/" component={HomePage}/>
-      <Route path="/hats" component={HatsPage}/>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   )
 }
