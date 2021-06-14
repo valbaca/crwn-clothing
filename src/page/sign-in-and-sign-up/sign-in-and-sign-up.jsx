1a03ade60a778c50e381ae0fd4ca8202e7d0a2d5
Valentin Baca <valbaca@gmail.com>
2021-06-13 17:45:04 -0700

    Store auth'd users into db

--- src/page/sign-in-and-sign-up/sign-in-and-sign-up.jsx
+++ src/page/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import SignIn from '../../components/sign-in/sign-in.jsx'
+import SignUp from '../../components/sign-up/sign-up.jsx'
 
 import './sign-in-and-sign-up.scss'
 
 const SignInAndSignUpPage = () => (
   <div className="sign-in-and-sign-up">
-  <SignIn/>
+    <SignIn />
+    <SignUp />
   </div>
 )
 
