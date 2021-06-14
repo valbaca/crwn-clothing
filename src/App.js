79e402afee376ea59f1e2e205d61274dc90b72e4
Valentin Baca <valbaca@gmail.com>
2021-06-13 17:24:35 -0700

    Storing user data to firestore, within page state

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/header/header.jsx'
 import SignInAndSignUpPage from './page/sign-in-and-sign-up/sign-in-and-sign-up.jsx'
 
 import './App.css'
-import { auth } from './firebase/firebase.utils.js'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils.js'
 
 class App extends React.Component {
   unsubscribeFromAuth = null
@@ -17,9 +17,20 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      console.log(user)
-      this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        })
+      } else {
+        this.setState({ currentUser: userAuth })
+      }
     })
   }
 
