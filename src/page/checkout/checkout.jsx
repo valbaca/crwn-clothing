996e736ab862627fd02da332a0dc5ba49b9ff810
Valentin Baca <valbaca@gmail.com>
2021-07-08 16:15:22 -0700

    formatting

--- src/page/checkout/checkout.jsx
+++ src/page/checkout/checkout.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
-import { selectCartItems, selectCartTotal } from '../../redux/cart/cart-selectors'
+import {
+  selectCartItems,
+  selectCartTotal
+} from '../../redux/cart/cart-selectors'
 
 import './checkout.scss'
 
-const CheckoutPage = ({cartItems, total}) => (
+const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
     <div className="checkout-header">
       <div className="header-block">
@@ -27,7 +30,9 @@ const CheckoutPage = ({cartItems, total}) => (
 
     {cartItems.map(cartItem => cartItem.name)}
 
-    <div className='total'><span>Total: ${total}</span></div>
+    <div className="total">
+      <span>Total: ${total}</span>
+    </div>
   </div>
 )
 
