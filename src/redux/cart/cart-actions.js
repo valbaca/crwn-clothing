6919e12027ebffc57a58785be1b7bc167a58f314
Valentin Baca <valbaca@gmail.com>
2021-07-08 17:24:45 -0700

    Impl add/remove item from checkout page

--- src/redux/cart/cart-actions.js
+++ src/redux/cart/cart-actions.js
@@ -1,11 +1,16 @@
-import CartActionTypes from "./cart-types";
+import CartActionTypes from './cart-types'
 
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN
 })
 
-export const addItem = (item) => ({
+export const addItem = item => ({
   type: CartActionTypes.ADD_ITEM,
+  payload: item
+})
+
+export const removeItem = item => ({
+  type: CartActionTypes.REMOVE_ITEM,
   payload: item
 })
 
