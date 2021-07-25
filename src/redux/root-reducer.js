fedb1dfa5aabc59e7de6b7cda5c691e0a0e7dc8c
Valentin Baca <valbaca@gmail.com>
2021-07-25 16:20:43 -0700

    create directory selector, reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux'
 import { persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import cartReducer from './cart/cart-reducer'
+import directoryReducer from './directory/directory-reducer'
 import userReducer from './user/user-reducer'
 
 const persistConfig = {
@@ -12,7 +13,8 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer
 })
 
 export default persistReducer(persistConfig, rootReducer)
