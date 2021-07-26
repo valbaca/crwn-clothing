9b4b199a758594f071837947914bfc7c236b74ca
Valentin Baca <valbaca@gmail.com>
2021-07-25 17:37:49 -0700

    collection items now show on collection pages

--- src/page/collection/collection.jsx
+++ src/page/collection/collection.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import CollectionItem from '../../components/collection-item/collection-item'
 import { selectCollection } from '../../redux/shop/shop-selectors'
 import './collection.scss'
 
-const CollectionPage = ({ collection }) => (
-  <div className="collection">
-    <h2>Collection page</h2>
-  </div>
-)
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  )
+}
 
 // ownProps allows us to use match.params.collectionId to get the :collectionId url param that's passed from ShopPage
 const mapStateToProps = (state, ownProps) => ({
