ab2cc5b2ef25a8a6965039d88b1f3a98515ce1b3
Valentin Baca <valbaca@gmail.com>
2021-07-25 17:24:03 -0700

    use lodash.memoize for selectCollection

--- src/redux/shop/shop-selectors.js
+++ src/redux/shop/shop-selectors.js
@@ -1,3 +1,4 @@
+import memoize from 'lodash.memoize'
 import { createSelector } from 'reselect'
 
 const COLLECTION_ID_MAP = {
@@ -15,9 +16,10 @@ export const selectCollections = createSelector(
   shop => shop.collections
 )
 
-export const selectCollection = collectionUrlParam =>
+export const selectCollection = memoize(collectionUrlParam =>
   createSelector([selectCollections], collections =>
     collections.find(
       collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
     )
   )
+)
