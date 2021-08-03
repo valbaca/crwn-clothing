6cb9feb294ce7275044133388a50b7221f91ed28
Valentin Baca <valbaca@gmail.com>
2021-08-02 21:18:54 -0700

    fix styling on custom buttons

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -2,7 +2,6 @@ import React from "react"
 import { connect } from "react-redux"
 import { addItem } from "../../redux/cart/cart-actions"
 import CustomButton from "../custom-button/custom-button"
-
 import "./collection-item.scss"
 
 const CollectionItem = ({ item, addItem }) => {
@@ -14,7 +13,11 @@ const CollectionItem = ({ item, addItem }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <CustomButton onClick={() => addItem(item)} inverted>
+      <CustomButton
+        className="custom-button"
+        onClick={() => addItem(item)}
+        inverted
+      >
         Add to cart
       </CustomButton>
     </div>
