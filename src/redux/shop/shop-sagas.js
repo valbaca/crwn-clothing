fd87e32effc7430a4256352d78e9075eb4ad1589
Valentin Baca <valbaca@gmail.com>
2021-09-03 10:49:21 -0700

    refactor shopSagas

--- src/redux/shop/shop-sagas.js
+++ src/redux/shop/shop-sagas.js
@@ -1,4 +1,4 @@
-import { call, put, takeLatest } from "redux-saga/effects"
+import { all, call, put, takeLatest } from "redux-saga/effects"
 import {
   convertCollectionsSnapshotToMap,
   firestore
@@ -22,4 +22,8 @@ export function* fetchCollectionsStart() {
     ShopActionTypes.FETCH_COLLECTIONS_START,
     fetchCollectionsAsync
   )
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionsStart)])
 }
