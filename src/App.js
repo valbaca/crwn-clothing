48e5e7c9c645b0a33a4a894622714309ae5be0ae
Valentin Baca <valbaca@gmail.com>
2021-09-12 18:55:00 -0700

    move App to functional component
    Using useDispatch, useSelector

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, { useEffect } from "react"
-import { connect } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { Redirect, Route, Switch } from "react-router-dom"
-import { createStructuredSelector } from "reselect"
 import "./App.css"
 import Header from "./components/header/header.jsx"
 import CheckoutPage from "./page/checkout/checkout.jsx"
 import HomePage from "./page/homepage/homepage.jsx"
 import ShopPage from "./page/shop/shop.jsx"
 import SignInAndSignUpPage from "./page/sign-in-and-sign-up/sign-in-and-sign-up.jsx"
-import { selectCollectionsForPreview } from "./redux/shop/shop-selectors.js"
 import { checkUserSession } from "./redux/user/user-actions.js"
 import { selectCurrentUser } from "./redux/user/user-selectors.js"
 
-const App = ({ checkUserSession, currentUser }) => {
+const App = () => {
+  const currentUser = useSelector(selectCurrentUser)
+  const dispatch = useDispatch()
+
   useEffect(() => {
-    checkUserSession()
-  }, [checkUserSession])
+    dispatch(checkUserSession())
+  }, [dispatch])
 
   return (
     <div>
@@ -36,13 +37,4 @@ const App = ({ checkUserSession, currentUser }) => {
   )
 }
 
-const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
-  collectionsArray: selectCollectionsForPreview
-})
-
-const mapDispatchToProps = (dispatch) => ({
-  checkUserSession: () => dispatch(checkUserSession())
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App
