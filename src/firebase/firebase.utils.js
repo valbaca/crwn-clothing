7d0fd8e50447cdf63ee950e801cbcd4dce47158e
Valentin Baca <valbaca@gmail.com>
2024-09-15 13:42:52 -0700

    firebase migration: part 1, compat

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
-import firebase from "firebase/app"
-import "firebase/auth"
-import "firebase/firestore"
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
 
 const config = {
   apiKey: "AIzaSyDnq4nU1J8Oi_7cIk4VV6LBs0GbPslE1TI",
