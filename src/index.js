82d6c6b6be50a47fc01a7d0d9cb904282bac734c
Valentin Baca <valbaca@gmail.com>
2024-09-15 12:35:08 -0700

    react-router v6 migration, part 1

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import App from "./App"
-import { BrowserRouter } from "react-router-dom"
 import { Provider } from "react-redux"
-import { store, persistor } from "./redux/store"
+import { BrowserRouter } from "react-router-dom"
+import { CompatRouter } from "react-router-dom-v5-compat"
 import { PersistGate } from "redux-persist/integration/react"
+import App from "./App"
+import { persistor, store } from "./redux/store"
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <PersistGate persistor={persistor}>
-          <App />
-        </PersistGate>
+        <CompatRouter>
+          <PersistGate persistor={persistor}>
+            <App />
+          </PersistGate>
+        </CompatRouter>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
